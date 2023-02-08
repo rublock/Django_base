ec5296e9b8518cab92508e6c635ab4adf56b41a9
hypermail <hypermail@yandex.ru>
2023-02-08 14:15:38 +0300

    get_context_data

--- mainapp/views.py
+++ mainapp/views.py
@@ -5,6 +5,13 @@ from django.views.generic import TemplateView
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
 
+    def get_context_data(self, **kwargs):
+        # get all previous data
+        context = super().get_context_data(**kwargs)
+        # create your own data
+        context["data_for_site"] = "some data to display from get_context_data()"
+        return context
 
+# get data from address panel
 def check_kwargs(request, **kwargs):
     return HttpResponse(f"kwargs:<br>{kwargs}")
