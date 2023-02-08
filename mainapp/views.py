e298d601dc4253b8c252a18a7c1fd4b9fb80284c
hypermail <hypermail@yandex.ru>
2023-02-08 11:45:31 +0300

    pages_static_media

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+
+def check_kwargs(request, **kwargs):
+    return HttpResponse(f"kwargs:<br>{kwargs}")
