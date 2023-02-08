b24c739c318b34440522632b545f4c7769e5a1a5
hypermail <hypermail@yandex.ru>
2023-02-08 22:06:57 +0300

    database on page display, admin, debug, req.txt

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,15 +1,14 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
+from mainapp import models
 
 
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
 
     def get_context_data(self, **kwargs):
-        # get all previous data
-        context = super().get_context_data(**kwargs)
-        # create your own data
-        context["data_for_site"] = "some data to display from get_context_data()"
+        context = super(MainPageView, self).get_context_data(**kwargs)
+        context["objects"] = models.DataBase.objects.all()
         return context
 
 # get data from address panel
